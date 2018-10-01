ed608e517c29c7a53d7a9113a221167147496fd7
Matthew <info@towersdesign.co.za>
2018-10-01 21:39:15 +0200

    Made changes to the customer form

--- Vidly2/ViewModels/CustomerFormViewModel.cs
+++ Vidly2/ViewModels/CustomerFormViewModel.cs
@@ -6,9 +6,9 @@ using Vidly2.Models;
 
 namespace Vidly2.ViewModels
 {
-    public class NewCustomerViewModel
+    public class CustomerFormViewModel
     {
-        public IEnumerable<MembershipType> membershipTypes { get; set; }
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
         public Customer Customer { get; set; }
 
     }
